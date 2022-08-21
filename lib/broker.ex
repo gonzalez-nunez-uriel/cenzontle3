ef1da378c3f5f7f8b93525e4f6f95b21ab623f76
Uriel Gonzalez Nunez <gonzalez.nunez.uriel@gmail.com>
2022-08-20 23:01:07 -0500

    Solved: how to handle multiple URL to same Broker.
    After carefull considerations this is the best I could figure out. I
    came up with an idea to set these things up when the user inputs these
    URLs into the system.

--- lib/broker.ex
+++ lib/broker.ex
@@ -8,12 +8,34 @@ defmodule Broker do
 
   """
 
+  # When the user input the URL, there should be a way for them to choose which briker to assign. Like this:
+
+  # +-------------------------------------------------+
+  # |          +-----------------------------------+  |
+  # |     URL: | <Query>                           |  |
+  # |          +-----------------------------------+  |
+  # |                                                 |
+  # |          +--------------+---+                   |
+  # |  Broker: | <BrokerName> | V |                   |
+  # |          +--------------+---+                   |
+  # |                                                 |
+  # + ----- <div with Broker specific configs> -------|
+  # | In StackOverflow, whether the accepted answer,  |
+  # | the most voted comment on the authors           |
+  # | response, etc should also be indexed            |
+  # |                                                 |
+  # +-------------------------------------------------+
+  # First time doing UI ascii art. Not bad.
+
+  # These is tedious and error prone, so a better alternative should be considered. One way would be by having each briker indicate what websites it can handle. StackOverflow and Stack Exchange have similar layouts. Maybe someone fygures out a generic that can handle reddit and other sites with a similar format. Remember, the whole point is not to constrain a power user. The arch needs to be so that things are customizable. Perhaps, have the drop down selector display all of the Broker that can handle that cite. If none, the user should be informed and allow them to use the generic Broker (the one that indexes everything in a page).
   @broker_registry %{
     "stackoverflow.com" => "StackOverflow",
     "hover.blog" => "Hover",
     "blog.appsignal.com" => "AppSignal",
     "www.reddit.com" => "Reddit",
-    "devrant.com" => "DevRant"
+    "devrant.com" => "DevRant",
+    "www.youtube.com" => "YouTube",
+    "youtube.com" => "YouTube"
   }
 
   # ~ DON'T FORGET THAT THE BROKER NEEDS TO REPORT BACK ONCE IT IS DONE
